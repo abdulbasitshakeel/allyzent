22bdd9533242cb92873ab27f169542301ceeddd4
Rabsha Shakeel <rabshasiddiqui@gmail.com>
2025-04-25 19:53:57 +0500

    donee

--- retail.php
+++ retail.php
@@ -34,16 +34,8 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="hero-work-content autoherosection">
-<<<<<<< Updated upstream
                         <h1 class="hero-work-heading">AI that drives more foot traffic to your business.</h1>
                         <p class="hero-work-description">AI Sales Consultant provides instant answers, including inventory details, and boosts foot traffic to your store.</p>
-=======
-                        <h1 class="hero-work-heading">AI that converts leads and makes you money.</h1>
-                        <p class="hero-work-description">
-                            AI that books test drives, service appointments, sends promotions, and makes sales—24/7, 
-                            with full context on your business.
-                        </p>
->>>>>>> Stashed changes
                         <div class="hero-work-divider"></div>
                         <a href="/" class="hero-work-btn">
                             <span class="text">Get a demo</span>
@@ -60,11 +52,7 @@
                 </div>
                 <div class="col-md-6">
                     <div class="showimagenew">
-<<<<<<< Updated upstream
                         <img src="assets/images/retail.jpg">
-=======
-                        <img src="assets/images/cars.jpg">
->>>>>>> Stashed changes
                     </div>
                 </div>
             </div>
@@ -91,20 +79,13 @@
     <section class="services-section">
         <div class="container">
             <div class="maindatahere">
-<<<<<<< Updated upstream
                 <h5>Allyzent's AI Solution</h5>
                 <h2 class="section-title">How Allyzent AI Supercharges Your Store's Success</h2>
                 <p class="section-subtitle">Allyzent's AI Sales Consultant is designed to engage customers and drive more foot traffic to your store.</p>
-=======
-                <h5>Podium's AI Solution</h5>
-                <h2 class="section-title">What Podium AI can do for you</h2>
-                <p class="section-subtitle">Podium's AI is built to talk to more leads, set more appointments, and follow up for your business with little oversight.</p>
->>>>>>> Stashed changes
             </div>
 
             <div class="services-grid">
                 <div class="service-card">
-<<<<<<< Updated upstream
                     <div class="icon">
                         <img src="assets/images/dataget.gif" />
                     </div>
@@ -117,21 +98,10 @@
                     </div>
                     <h3>Convert more leads.</h3>
                     <p>Shoppers don’t wait. Responding in under a minute increases your chance of a sale by over 45%. With the AI Sales Consultant, get instant, precise responses—24/7.</p>
-=======
-                    <div class="icon">🛠️</div>
-                    <h3>Capture every lead.</h3>
-                    <p>Never miss a lead again—see every call, text, website chat, review, and third-party app message—all from a consolidated inbox.</p>
-                </div>
-                <div class="service-card">
-                    <div class="icon">🎨</div>
-                    <h3>Convert more leads.</h3>
-                    <p>Podium AI responds to leads in 1 minute to boost your chance of a sale by >45%. Help your customers immediately, accurately, and 24/7.</p>
->>>>>>> Stashed changes
                 </div>
             </div>
             <div class="services-grid" style="margin-top: 25px;">
                 <div class="service-card">
-<<<<<<< Updated upstream
                     <div class="icon">
                         <img src="assets/images/social.gif" style="padding:46px 0;width: 170px;" />
                     </div>
@@ -144,16 +114,6 @@
                     </div>
                     <h3>Get found. Get chosen.</h3>
                     <p>More reviews lead to more sales. Our AI automatically requests feedback after each transaction and engages with customers on review platforms, helping you climb the Google rankings and attract more business.</p>
-=======
-                    <div class="icon">⚙️</div>
-                    <h3>Drive repeat business.</h3>
-                    <p>Let AI put in the work to drive repeat sales. Our AI Employee proactively follows up with customers at the right time to guide them to a sale.</p>
-                </div>
-                <div class="service-card">
-                    <div class="icon">🚀</div>
-                    <h3>Get found. Get chosen.</h3>
-                    <p>More reviews = more business. Our AI requests reviews after each sale and responds to customers on review sites, helping you rank high on Google.</p>
->>>>>>> Stashed changes
                 </div>
             </div>
         </div>
@@ -161,51 +121,30 @@
     <section class="timeline-section">
         <div class="container">
             <div class="maindatahere  text-center">
-<<<<<<< Updated upstream
                 <h5>Allyzent's AI Solution</h5>
                 <h2 class="section-title">AI that drives customers to your store and website.</h2>
                 <!-- <p class="section-subtitle">Allyzent's AI is built to talk to more leads, set more appointments, and follow up for your business with little oversight.</p> -->
-=======
-                <h5>Podium's AI Solution</h5>
-                <h2 class="section-title">Expand your team exponentially with Podium's AI Employee</h2>
-                <!-- <p class="section-subtitle">Podium's AI is built to talk to more leads, set more appointments, and follow up for your business with little oversight.</p> -->
->>>>>>> Stashed changes
             </div>
             <div class="timeline">
                 <div class="timeline-item">
                     <div class="timeline-marker"></div>
                     <div class="timeline-content">
-<<<<<<< Updated upstream
                         <h3>Boost sales round the clock.</h3>
                         <p>Our AI, dedicated to customer satisfaction and boosting sales 24/7, can increase your revenue by up to 30%.</p>
-=======
-                        <h3>Boosts your sales</h3>
-                        <p>Focused on customer satisfaction and driving sales, our AI Employee increases your bottom line by up to 30%.</p>
->>>>>>> Stashed changes
                     </div>
                 </div>
                 <div class="timeline-item">
                     <div class="timeline-marker"></div>
                     <div class="timeline-content">
-<<<<<<< Updated upstream
                         <h3>Hire a product expert</h3>
                         <p>Our AI, trained like an expert consultant, provides detailed answers on inventory, pricing, and more—empowering your team with knowledge.</p>
-=======
-                        <h3>Knows your industry</h3>
-                        <p>Pre-trained on 10+ years of industry expertise, our AI Employee boosts customer engagement rates by 24%.</p>
->>>>>>> Stashed changes
                     </div>
                 </div>
                 <div class="timeline-item">
                     <div class="timeline-marker"></div>
                     <div class="timeline-content">
-<<<<<<< Updated upstream
                         <h3>Tailored and fast</h3>
                         <p>Customized to your products, prices, and policies, our AI is designed for your business and seamlessly set up by our experts.</p>
-=======
-                        <h3>Sets up quickly</h3>
-                        <p>Customized to your policies and services, our AI Employee is easy to set up, tailored to your needs by our experts and your guidance.</p>
->>>>>>> Stashed changes
                     </div>
                 </div>
             </div>
@@ -214,7 +153,6 @@
     <section class="testimonial-slider" style="padding-bottom: 130px;">
         <div class="container">
             <div class="maindatahere text-center">
-<<<<<<< Updated upstream
                 <h5>Allyzent's AI Solution</h5>
                 <h2 class="section-title">Over 20,000 retailers rely on <br>Allyzent for success.</h2>
             </div>
@@ -226,38 +164,16 @@
                 <div class="testimonial-card">
                     <p>“Boost in customer engagement after integrating AI.”</p>
                     <span>- Samuel Grant – HomeTech Solutions</span>
-=======
-                <h5>Podium's AI Solution</h5>
-                <h2 class="section-title">Over 100,000 businesses<br> trust Podium.</h2>
-            </div>
-            <div class="slider-track" style="padding-top: 30px;">
-                <div class="testimonial-card">
-                    <p>“[Our AI Employee] is excellent. We were going to hire a customer service rep, but now we don't have to.”</p>
-                    <span>- Gannon Kennedy, Epione</span>
-                </div>
-                <div class="testimonial-card">
-                    <p>“Podium AI has 2x’d our lead response and lead closing rates.”</p>
-                    <span>- Ben Johnson, Stuart Powell Ford Mazda</span>
->>>>>>> Stashed changes
                 </div>
             </div>
             <div class="slider-track" style="margin-top: 30px;">
                 <div class="testimonial-card">
-<<<<<<< Updated upstream
                     <p>Reduction in average response time, from over 16 minutes to under 6 minutes.</p>
                     <span>- Ethan Roberts – AutoExperts</span>
                 </div>
                 <div class="testimonial-card">
                     <p>“We’re seeing more conversions because it's simpler to assist customers with their questions online, rather than letting them leave the website.”</p>
                     <span>- Charlotte Harris – Luxury Interior Designs</span>
-=======
-                    <p><strong>72%</strong> — “Podium’s AI has cut response time from 5 mins to 2 mins.”</p>
-                    <span>- Auto Companies using Podium</span>
-                </div>
-                <div class="testimonial-card">
-                    <p>“The AI Employee can start/end the chat and book into ServiceTitan. It's doing almost all the work.”</p>
-                    <span>- Reed Borton, Premier Heating and Air</span>
->>>>>>> Stashed changes
                 </div>
             </div>
         </div>            
