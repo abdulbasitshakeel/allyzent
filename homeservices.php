fa4270ed00b9a84aad66a4617c7579d67694f831
Rabsha Shakeel <rabshasiddiqui@gmail.com>
2025-04-25 19:50:55 +0500

    update

--- homeservices.php
+++ homeservices.php
@@ -5,7 +5,10 @@
 }
 .autoherosection {
     text-align: left !important;
+<<<<<<< Updated upstream
     padding-top: 35px;
+=======
+>>>>>>> Stashed changes
 }
 .autoherosection h1 {
     font-size: 74px;
@@ -35,8 +38,16 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="hero-work-content autoherosection">
+<<<<<<< Updated upstream
                         <h1 class="hero-work-heading">AI CSR for calls and bookings.</h1>
                         <p class="hero-work-description">AI CSR handles service inquiries and schedules appointments, even after hours, allowing you to focus on the job at hand.</p>
+=======
+                        <h1 class="hero-work-heading">AI that converts leads and makes you money.</h1>
+                        <p class="hero-work-description">
+                            AI that books test drives, service appointments, sends promotions, and makes sales—24/7, 
+                            with full context on your business.
+                        </p>
+>>>>>>> Stashed changes
                         <div class="hero-work-divider"></div>
                         <a href="/" class="hero-work-btn">
                             <span class="text">Get a demo</span>
@@ -53,7 +64,11 @@
                 </div>
                 <div class="col-md-6">
                     <div class="showimagenew">
+<<<<<<< Updated upstream
                         <img src="assets/images/aihomeservice.jpg">
+=======
+                        <img src="assets/images/cars.jpg">
+>>>>>>> Stashed changes
                     </div>
                 </div>
             </div>
@@ -80,13 +95,20 @@
     <section class="services-section">
         <div class="container">
             <div class="maindatahere">
+<<<<<<< Updated upstream
                 <h5>Allyzent AI Solution</h5>
                 <h2 class="section-title">Always be available for every call.</h2>
                 <p class="section-subtitle">Say goodbye to stopping the truck. Your AI CSR handles calls 24/7, takes detailed notes, and delivers excellent customer service.</p>
+=======
+                <h5>Podium's AI Solution</h5>
+                <h2 class="section-title">What Podium AI can do for you</h2>
+                <p class="section-subtitle">Podium's AI is built to talk to more leads, set more appointments, and follow up for your business with little oversight.</p>
+>>>>>>> Stashed changes
             </div>
 
             <div class="services-grid">
                 <div class="service-card">
+<<<<<<< Updated upstream
                     <div class="icon">
                         <img src="assets/images/dataget.gif" />
                     </div>
@@ -99,10 +121,21 @@
                     </div>
                     <h3>Automated Appointment Scheduling.</h3>
                     <p>Seamlessly manage bookings with our AI CSR, ensuring efficient scheduling even during off-hours.</p>
+=======
+                    <div class="icon">🛠️</div>
+                    <h3>Capture every lead.</h3>
+                    <p>Never miss a lead again—see every call, text, website chat, review, and third-party app message—all from a consolidated inbox.</p>
+                </div>
+                <div class="service-card">
+                    <div class="icon">🎨</div>
+                    <h3>Convert more leads.</h3>
+                    <p>Podium AI responds to leads in 1 minute to boost your chance of a sale by >45%. Help your customers immediately, accurately, and 24/7.</p>
+>>>>>>> Stashed changes
                 </div>
             </div>
             <div class="services-grid" style="margin-top: 25px;">
                 <div class="service-card">
+<<<<<<< Updated upstream
                     <div class="icon">
                         <img src="assets/images/social.gif" style="padding:46px 0;width: 170px;" />
                     </div>
@@ -115,6 +148,16 @@
                     </div>
                     <h3>Customer Service Excellence.</h3>
                     <p>Provide consistent and professional customer service with AI-powered interactions, ensuring a smooth experience for your clients.</p>
+=======
+                    <div class="icon">⚙️</div>
+                    <h3>Drive repeat business.</h3>
+                    <p>Let AI put in the work to drive repeat sales. Our AI Employee proactively follows up with customers at the right time to guide them to a sale.</p>
+                </div>
+                <div class="service-card">
+                    <div class="icon">🚀</div>
+                    <h3>Get found. Get chosen.</h3>
+                    <p>More reviews = more business. Our AI requests reviews after each sale and responds to customers on review sites, helping you rank high on Google.</p>
+>>>>>>> Stashed changes
                 </div>
             </div>
         </div>
@@ -122,30 +165,51 @@
     <section class="timeline-section">
         <div class="container">
             <div class="maindatahere  text-center">
+<<<<<<< Updated upstream
                 <h5>Allyzent’s AI Solution</h5>
                 <h2 class="section-title">AI CSR that books services while you're on the job.</h2>
                 <!-- <p class="section-subtitle">Allyzent's AI is built to talk to more leads, set more appointments, and follow up for your business with little oversight.</p> -->
+=======
+                <h5>Podium's AI Solution</h5>
+                <h2 class="section-title">Expand your team exponentially with Podium's AI Employee</h2>
+                <!-- <p class="section-subtitle">Podium's AI is built to talk to more leads, set more appointments, and follow up for your business with little oversight.</p> -->
+>>>>>>> Stashed changes
             </div>
             <div class="timeline">
                 <div class="timeline-item">
                     <div class="timeline-marker"></div>
                     <div class="timeline-content">
+<<<<<<< Updated upstream
                         <h3>AI that seals the deal quickly.</h3>
                         <p>Our AI boosts your service booking rate by three times, prioritizing customer satisfaction and increasing sales.</p>
+=======
+                        <h3>Boosts your sales</h3>
+                        <p>Focused on customer satisfaction and driving sales, our AI Employee increases your bottom line by up to 30%.</p>
+>>>>>>> Stashed changes
                     </div>
                 </div>
                 <div class="timeline-item">
                     <div class="timeline-marker"></div>
                     <div class="timeline-content">
+<<<<<<< Updated upstream
                         <h3>Industry-Specific Expertise</h3>
                         <p>Our AI Employee, trained with over 10 years of industry knowledge, increases customer engagement by 24%.</p>
+=======
+                        <h3>Knows your industry</h3>
+                        <p>Pre-trained on 10+ years of industry expertise, our AI Employee boosts customer engagement rates by 24%.</p>
+>>>>>>> Stashed changes
                     </div>
                 </div>
                 <div class="timeline-item">
                     <div class="timeline-marker"></div>
                     <div class="timeline-content">
+<<<<<<< Updated upstream
                         <h3>Quick and Easy Setup</h3>
                         <p>Tailored to your unique policies and services, our AI Employee is set up quickly with the help of our experts and your input.</p>
+=======
+                        <h3>Sets up quickly</h3>
+                        <p>Customized to your policies and services, our AI Employee is easy to set up, tailored to your needs by our experts and your guidance.</p>
+>>>>>>> Stashed changes
                     </div>
                 </div>
             </div>
@@ -154,6 +218,7 @@
     <section class="testimonial-slider" style="padding-bottom: 130px;">
         <div class="container">
             <div class="maindatahere text-center">
+<<<<<<< Updated upstream
                 <h5>Allyzent’s AI Solution</h5>
                 <h2 class="section-title">8,000+ home services<br> trust Allyzent.</h2>
             </div>
@@ -165,16 +230,38 @@
                 <div class="testimonial-card">
                     <p>“With Podium AI, our lead engagement and closing rates have significantly improved.”</p>
                     <span>- Mark Thompson, Elite Auto Service</span>
+=======
+                <h5>Podium's AI Solution</h5>
+                <h2 class="section-title">Over 100,000 businesses<br> trust Podium.</h2>
+            </div>
+            <div class="slider-track" style="padding-top: 30px;">
+                <div class="testimonial-card">
+                    <p>“[Our AI Employee] is excellent. We were going to hire a customer service rep, but now we don't have to.”</p>
+                    <span>- Gannon Kennedy, Epione</span>
+                </div>
+                <div class="testimonial-card">
+                    <p>“Podium AI has 2x’d our lead response and lead closing rates.”</p>
+                    <span>- Ben Johnson, Stuart Powell Ford Mazda</span>
+>>>>>>> Stashed changes
                 </div>
             </div>
             <div class="slider-track" style="margin-top: 30px;">
                 <div class="testimonial-card">
+<<<<<<< Updated upstream
                     <p><strong>75%</strong> — “Podium AI has cut our response time dramatically, from 6 minutes to just under 3 minutes.”</p>
                     <span>- Leading Service Providers Using Podium</span>
                 </div>
                 <div class="testimonial-card">
                     <p>“The AI Employee handles chat interactions and integrates bookings seamlessly into our scheduling system, making everything more efficient.”</p>
                     <span>- John Adams, Premier Home Services</span>
+=======
+                    <p><strong>72%</strong> — “Podium’s AI has cut response time from 5 mins to 2 mins.”</p>
+                    <span>- Auto Companies using Podium</span>
+                </div>
+                <div class="testimonial-card">
+                    <p>“The AI Employee can start/end the chat and book into ServiceTitan. It's doing almost all the work.”</p>
+                    <span>- Reed Borton, Premier Heating and Air</span>
+>>>>>>> Stashed changes
                 </div>
             </div>
         </div>            
